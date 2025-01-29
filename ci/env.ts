0e5618860b67a97ed9d6a6d811b6d0738d3cb54f
Terrence Harvey-Franklin <dev@eatmycode.io>
2025-01-28 22:18:57 -0500

    update build process

--- ci/env.ts
+++ ci/env.ts
@@ -1,6 +1,7 @@
 import { $ } from 'bun'; // Shell execution via JS
 
 let env: any = {};
+let api: any = {};
 
 env = await $`doppler secrets -p frtnd -c prd_marquelmedia download --no-file --format json`.json();
 
@@ -8,14 +9,9 @@ Bun.write(`.env.marquelmedia`, JSON.stringify(env));
 
 // Apply Environment Vars
 await $`echo "Applying Environment Vars"`;
-for await (let file of $`grep -Rl 'APP\\|THEME\\|CLIENT\\|TENANT\\|MKTG\\|MM' ./`.lines()) {
-  if (file) {
-    let content = await Bun.file(file).text();
-    Object.keys(env).forEach(async (secret) => {
-      content = content.replace(new RegExp(secret, 'g'), env[secret]);
-    });
-    await Bun.write(file, content);
-  }
-}
+
+api = JSON.parse(await Bun.file('./js/bcknd.spec.json').text());
+api.api.v1.version = env.MM_VERSION;
+await Bun.write('./js/bcknd.spec.json', JSON.stringify(api));
 
 await $`echo "Done!\n"`;
