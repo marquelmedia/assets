47327db6111d37a15389d768e4bc5d94f01c0cf6
Terrence Harvey-Franklin <dev@eatmycode.io>
2025-01-29 12:46:45 -0500

    update bcknd docs

--- ci/env.ts
+++ ci/env.ts
@@ -3,6 +3,7 @@ import { $ } from 'bun'; // Shell execution via JS
 let bcknd: any = {};
 let api: any = {};
 
+/*
 bcknd = await $`doppler secrets -p bcknd -c prd_marquelmedia download --no-file --format json`.json();
 
 //Bun.write(`.env.marquelmedia`, JSON.stringify(env));
@@ -15,3 +16,4 @@ api.api.v1.version = bcknd.APP_VERSION;
 
 await Bun.write('./js/bcknd.spec.json', JSON.stringify(api, null, 2));
 await $`echo "Done!\n"`;
+*/
